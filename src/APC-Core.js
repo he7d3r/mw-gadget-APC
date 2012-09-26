30940bda9f8bf515f419f8b0ee09500a445ff6c0
Helder <he7d3r@gmail.com>
2012-09-26 21:13:19 +0000

    AWB --> APC

--- src/APC-Core.js
+++ src/APC-Core.js
@@ -4,28 +4,28 @@
  * por meio do processamento de listas de regras do tipo "localizar e substituir"
  * (análoga à existente no AutoWikiBrowser)
  * Ver também:
- ** [[WP:Projetos/AWB/Script]] (documentação)
+ ** [[WP:Scripts/APC]] (documentação)
  ** [[w:User:!Silent/correções.js]]
  ** [[w:WP:Projetos/Check Wikipedia/AWB]]
  ** [[w:WP:Projetos/AWB]]
  ** http://autowikibrowser.svn.sourceforge.net/viewvc/autowikibrowser/AWB/WikiFunctions/ReplaceSpecial/Rule.cs?view=markup
  * @author: [[User:Helder.wiki]]
- * @tracking: [[Special:GlobalUsage/User:Helder.wiki/Tools/AWB/SearchAndReplace.js]] ([[File:User:Helder.wiki/Tools/AWB/SearchAndReplace.js]])
+ * @tracking: [[Special:GlobalUsage/User:Helder.wiki/Tools/APC.js]] ([[File:User:Helder.wiki/Tools/APC.js]])
  */
-/*global jQuery, mediaWiki, AWB */
+/*global jQuery, mediaWiki, APC */
 
-if ( window.AWB === undefined ) {
-	window.AWB = {};
+if ( window.APC === undefined ) {
+	window.APC = {};
 }
-(function ($, mw, AWB) {
+(function ($, mw, APC) {
 'use strict';
 
-$.extend( AWB, $.extend( {
-	version: '0.24',
+$.extend( APC, $.extend( {
+	version: '0.25',
 	text: '', // This will store the text to which the rules will be applied
 	allowFunctionTests: false, // TODO: Do we need this?
 	allowOnlyInsideTemplates: false, // TODO: Implement this
-	hasUserInterface: true, // TODO: Improve the fake "special" page [[WP:Projetos/AWB/Script]]
+	hasUserInterface: true, // TODO: Improve the fake "special" page [[WP:Scripts/APC]]
 	/**
 	 * @type Array.{{
 		name: {string}, // The name of the rule
@@ -39,21 +39,21 @@ $.extend( AWB, $.extend( {
 		sub: [] // an array of objects like this
 	}}
 	 * The list of rules used by the tool
-	 * FIXME: Convert this to a function such as AWB.addRules( {...} ),
+	 * FIXME: Convert this to a function such as APC.addRules( {...} ),
 	 * so that the list of rules can be defined inside a mw.loader.using()
 	 * having this JS module as dependency (check the toolbar API for examples)
 	 */
 	rules: []
-}, AWB ) );
+}, APC ) );
 
-// AWB.Rule.prototype = '...'; // TODO: Implement default values in some prototype?
+// APC.Rule.prototype = '...'; // TODO: Implement default values in some prototype?
 
 /**
  * Loop over all rules and subrules, applying those which are enabled
  * @param rules The list of rules
- * FIXME: Guardar o texto em uma variável local em vez de AWB.text, para otimizar
+ * FIXME: Guardar o texto em uma variável local em vez de APC.text, para otimizar
  */
-AWB.processRules = function (rules) {
+APC.processRules = function (rules) {
 	var	i, length, r, times, reKeyWords, temp,
 		keys = [],
 		// See also http://autowikibrowser.svn.sourceforge.net/viewvc/autowikibrowser/AWB/WikiFunctions/Tools.cs?revision=8179&view=markup#l536
@@ -77,15 +77,15 @@ AWB.processRules = function (rules) {
 		if (r.enabled !== false
 			&& (
 				!r.ifhas
-				|| (typeof r.ifhas === 'string' && AWB.text.indexOf(r.ifhas) !== -1)
-				|| (AWB.allowFunctionTests && $.isFunction(r.ifhas) && r.ifhas(AWB.text))
-				|| (typeof r.ifhas === 'object' && r.ifhas.test(AWB.text))
+				|| (typeof r.ifhas === 'string' && APC.text.indexOf(r.ifhas) !== -1)
+				|| (APC.allowFunctionTests && $.isFunction(r.ifhas) && r.ifhas(APC.text))
+				|| (typeof r.ifhas === 'object' && r.ifhas.test(APC.text))
 			)
 			&& (
 				!r.ifnot
-				|| (typeof r.ifnot === 'string' && AWB.text.indexOf(r.ifnot) === -1)
-				|| (AWB.allowFunctionTests && $.isFunction(r.ifnot) && !r.ifnot(AWB.text))
-				|| (typeof r.ifnot === 'object' && !r.ifnot.test(AWB.text))
+				|| (typeof r.ifnot === 'string' && APC.text.indexOf(r.ifnot) === -1)
+				|| (APC.allowFunctionTests && $.isFunction(r.ifnot) && !r.ifnot(APC.text))
+				|| (typeof r.ifnot === 'object' && !r.ifnot.test(APC.text))
 
 			)
 		) {
@@ -107,19 +107,19 @@ AWB.processRules = function (rules) {
 						: r.num > 100
 							? 100
 							: r.num;
-					temp = AWB.text;
+					temp = APC.text;
 					while( times > 0){
-						AWB.text = AWB.text.replace( r.find, r.replace );
+						APC.text = APC.text.replace( r.find, r.replace );
 						times--;
 					}
-					if( temp !== AWB.text ){
+					if( temp !== APC.text ){
 						mw.log( r.find, r.replace );
 					}
 				}
 				if ($.isArray(r.sub) && r.sub.length) {
-					AWB.processRules(r.sub);
+					APC.processRules(r.sub);
 				}
-			} else if (AWB.allowOnlyInsideTemplates && r.where === 'templates') {
+			} else if (APC.allowOnlyInsideTemplates && r.where === 'templates') {
 				// TODO: Implement this for non-nested templates!
 				// TODO: How to allow nested templates? E.g.: {{info|param={{format|...}}|param=...}}
 				mw.log('Invalid value: where=templates is not available yet.');
@@ -130,37 +130,37 @@ AWB.processRules = function (rules) {
 	}
 };
 
-AWB.addAWBToToolbar = function () {
+APC.addAPCToToolbar = function () {
 	var	i,
-		summaryText = ' +[[WP:Projetos/AWB/Script|tarefas repetitivas]] (v' +
-					AWB.version + '/' + AWB.rulesVersion + ')',
+		summaryText = ' +[[WP:Scripts/APC|correções automáticas]] (v' +
+					APC.version + '/' + APC.rulesVersion + ')',
 		$sumField = $('#wpSummary'),
 		executeGroup = function( i ){
 			return function() {
-				AWB.text = AWB.$target.val();
-				AWB.processRules(AWB.rules[i]);
-				AWB.$target.val(AWB.text);
+				APC.text = APC.$target.val();
+				APC.processRules(APC.rules[i]);
+				APC.$target.val(APC.text);
 				$sumField.val( $sumField.val() + summaryText );
 			};
 		},
 		mainGroupsOfFixes = { // {}
-			'awb-fixes-all' : {
+			'APC-fixes-all' : {
 				label: 'Todas',
 				action: {
 					type: 'callback',
 					execute: function() {
-						AWB.text = AWB.$target.val();
-						AWB.processRules(AWB.rules);
-						AWB.$target.val(AWB.text);
+						APC.text = APC.$target.val();
+						APC.processRules(APC.rules);
+						APC.$target.val(APC.text);
 						$sumField.val( $sumField.val() + summaryText );
 					}
 				}
 			}
 		};
-	for(i=0;i<AWB.rules.length;i++){
-		mainGroupsOfFixes[ 'awb-fixes-' + i ] = {
-			label: AWB.rules[i].name +
-				(AWB.rules[i].enabled === false? ' (desativada temporariamente)' : ''),
+	for(i=0;i<APC.rules.length;i++){
+		mainGroupsOfFixes[ 'APC-fixes-' + i ] = {
+			label: APC.rules[i].name +
+				(APC.rules[i].enabled === false? ' (desativada temporariamente)' : ''),
 			action: {
 				type: 'callback',
 				execute: executeGroup(i)
@@ -168,19 +168,18 @@ AWB.addAWBToToolbar = function () {
 		};
 	}
 
-	mw.log( 'Serão inseridos os botões do AWB' );
 	$( '#wpTextbox1' ).wikiEditor( 'addToToolbar', {
 		section: 'advanced',
 		groups: {
-			awb: {
-				label: 'AWB',
+			APC: {
+				label: 'APC',
 				tools: {
-					'awb-fixes-heading': {
+					'apc-fixes-heading': {
 						label: 'Correções',
 						type: 'select',
 						list: mainGroupsOfFixes
 					},
-					'awb-report-a-bug' : {
+					'apc-report-a-bug' : {
 						label: 'Informar um erro',
 						type: 'button',
 						// Icon by [[commons:User:Medium69]]
@@ -188,13 +187,13 @@ AWB.addAWBToToolbar = function () {
 						action: {
 							type: 'callback',
 							execute: function() {
-								var url = mw.util.wikiGetlink( 'Wikipédia Discussão:Projetos/AWB/Script' ) + '?' +
+								var url = mw.util.wikiGetlink( 'Wikipédia Discussão:Scripts/APC' ) + '?' +
 									$.param({
 										action: 'edit',
 										section: 'new',
-										preloadtitle: '[BUG] (v' + AWB.version + '/' +
-											AWB.rulesVersion + ') [[' + mw.config.get('wgPageName').replace(/_/g, ' ') + ']]',
-										preload: 'WP:Projetos/AWB/Script/Bug'
+										preloadtitle: '[BUG] (v' + APC.version + '/' +
+											APC.rulesVersion + ') [[' + mw.config.get('wgPageName').replace(/_/g, ' ') + ']]',
+										preload: 'WP:Scripts/APC/Bug'
 									});
 								window.open( url );
 							}
@@ -211,7 +210,7 @@ AWB.addAWBToToolbar = function () {
  * @param rules The list of rules
  * @return {jQuery} The jQuery object correspoding to the HTML of the requested
  */
-AWB.getRulesHTML = function (rules, visible) {
+APC.getRulesHTML = function (rules, visible) {
 	var i, length, r, $li,
 	$ul = $('<ul></ul>').toggle( visible || false );
 
@@ -222,11 +221,11 @@ AWB.getRulesHTML = function (rules, visible) {
 			.attr('title', 'Localizar:\n' + r.find + '\n\nSubstituir por:\n' + r.replace )
 			.text( r.name || 'Rule' );
 		if (r.enabled === false) {
-			$li.addClass('awb-disabled')
+			$li.addClass('apc-disabled')
 				.attr('title', 'Esta regra está desativada');
 		}
 		if ($.isArray(r.sub) && r.sub.length) {
-			$li.append(AWB.getRulesHTML(r.sub))
+			$li.append(APC.getRulesHTML(r.sub))
 				.addClass('list-toggle');
 		}
 		$li.appendTo($ul);
@@ -237,48 +236,48 @@ AWB.getRulesHTML = function (rules, visible) {
 /**
  * Execute the script when in edit mode
  */
-AWB.run = function () {
+APC.run = function () {
 	var $rules, $list, versionHTML, $button;
 
 	if ($.inArray(mw.config.get('wgAction'), ['edit', 'submit']) !== -1 ) {
-		AWB.$target = $('#wpTextbox1');
+		APC.$target = $('#wpTextbox1');
 
 		/* Make sure the required modules are available and then customize the toolbar */
 		mw.loader.using( 'user.options', function () {
 			if ( mw.user.options.get('usebetatoolbar') ) {
-				mw.loader.using( 'ext.wikiEditor.toolbar', AWB.addAWBToToolbar );
+				mw.loader.using( 'ext.wikiEditor.toolbar', APC.addAPCToToolbar );
 			} else{
 				// TODO: Improve support for old toolbar?
 				$( mw.util.addPortletLink(
 					'p-tb',
 					'#',
-					'Formatar com AWB',
-					'#ca-awb',
+					'Formatar com APC',
+					'#ca-APC',
 					'Formata o código wiki da página de acordo com as regras estabelecidas no código do script'
 				) ).click( function (e) {
 					e.preventDefault();
-					AWB.text = AWB.$target.val();
-					AWB.processRules(AWB.rules);
-					AWB.$target.val( AWB.text );
+					APC.text = APC.$target.val();
+					APC.processRules(APC.rules);
+					APC.$target.val( APC.text );
 				} );
 			}
 		} );
 
-	} else if ( mw.config.get('wgPageName') === 'Wikipédia:Projetos/AWB/Script' && $.inArray(mw.config.get('wgAction'), ['view', 'purge']) !== -1 ) {
+	} else if ( mw.config.get('wgPageName') === 'Wikipédia:Scripts/APC' && $.inArray(mw.config.get('wgAction'), ['view', 'purge']) !== -1 ) {
 		// TODO: Implement a true user interface on this "special page"
-		if (AWB.hasUserInterface) {
-			mw.util.addCSS('.awb-disabled{ color: red;} li{cursor: default;} .list-toggle{ cursor:pointer;}');
-			$rules = $('#awb-search-and-replace-rules');
+		if (APC.hasUserInterface) {
+			mw.util.addCSS('.apc-disabled{ color: red;} li{cursor: default;} .list-toggle{ cursor:pointer;}');
+			$rules = $('#apc-search-and-replace-rules');
 			if(!$rules.length) {
 				$rules = $('#mw-content-text');
 			}
-			versionHTML = '<p>Observação: esta é a versão ' + AWB.rulesVersion +
-				' da lista de regras (gerada pela versão ' + AWB.version + ' do script).</p>';
+			versionHTML = '<p>Observação: esta é a versão ' + APC.rulesVersion +
+				' da lista de regras (gerada pela versão ' + APC.version + ' do script).</p>';
 			$button = $('<input type="button" value="Expandir tudo" title="Expandir todos os itens"/>')
 				.click( function(){
 					$rules.find('ul').show();
 				});
-			$list = AWB.getRulesHTML(AWB.rules, true)
+			$list = APC.getRulesHTML(APC.rules, true)
 				.on('click', '.list-toggle', function (e) {
 					e.stopPropagation();
 					if ( $(e.target).hasClass('list-toggle') ){
@@ -295,6 +294,6 @@ AWB.run = function () {
 	}
 };
 
-$(AWB.run);
+$(APC.run);
 
-}(jQuery, mediaWiki, AWB));
+}(jQuery, mediaWiki, APC));
