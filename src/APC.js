1239dc0adc32c6d87c6adcc6d80f17c29a5658a3
Helder <he7d3r@gmail.com>
2012-11-01 18:03:54 +0000

    Parece que o https://bugzilla.wikimedia.org/show_bug.cgi?id=40288 está resolvido... (o código aparece na [https://bits.wikimedia.org/pt.wikipedia.org/load.php?modules=ext.gadget.APCCore])

--- src/APC.js
+++ src/APC.js
@@ -4,18 +4,11 @@
  * @author: [[User:Helder.wiki]]
  * @tracking: [[Special:GlobalUsage/User:Helder.wiki/Tools/APC.js]] ([[File:User:Helder.wiki/Tools/APC.js]])
  */
-/*jslint white: true, browser: true, devel: true */
-/*global mediaWiki, jQuery, importScript */
+/*jslint white: true */
+/*global mediaWiki, jQuery */
 
 (function (mw, $) {
 'use strict';
-
-var debug = mw.config.get( 'wgUserName' ) === 'Helder.wiki'
-	&& ! /\.js$/.test( mw.config.get( 'wgTitle' ) );
-
-if ( debug && new Date() >= Date.parse('November 3, 2012') ) {
-	alert( 'Atualizar MediaWiki:Gadget-APC.js (resolveram o bug 40288?)' );
-}
 
 if ( $.inArray( mw.config.get( 'wgAction' ), [ 'edit', 'submit' ] ) !== -1
 	|| (
@@ -24,17 +17,7 @@ if ( $.inArray( mw.config.get( 'wgAction' ), [ 'edit', 'submit' ] ) !== -1
 	)
 ) {
 	mw.loader.using( 'ext.gadget.APCList', function(){
-		if ( window.APC === undefined ){
-			if ( debug ) {
-				alert( 'Bug 40288! https://bits.wikimedia.org/pt.wikipedia.org/load.php?modules=ext.gadget.APCList' );
-			}
-			$.getScript( '//pt.wikipedia.org/w/index.php?title=MediaWiki:Gadget-APC.js/List.js&action=raw&ctype=text/javascript&smaxage=21600&maxage=86400' )
-			.done(function(){
-				importScript( 'MediaWiki:Gadget-APC.js/Core.js' );
-			});
-		} else {
-			mw.loader.load( 'ext.gadget.APCCore' );
-		}
+		mw.loader.load( 'ext.gadget.APCCore' );
 	} );
 }
 
