58c51df264eba97eed8825533d20eac81720885f
Helder <he7d3r@gmail.com>
2013-08-14 21:04:28 +0000

    Versão 3.1.34: corrigir primeiro os width's cuja unidade não seja pixels (ver https://pt.wikipedia.org/wiki/Wikipédia Discussão:Scripts/APC#.5BBUG.5D (v0.36/3.1.33) Campeonato Brasileiro de Futebol de 2004 - Série A)

--- src/APC-List.js
+++ src/APC-List.js
@@ -17,7 +17,7 @@
 var addDefaultRules = function(){
 'use strict';
 
-APC.rulesVersion = '3.1.33';
+APC.rulesVersion = '3.1.34';
 APC.addRules( [{
 	name: 'Iniciando',
 	ifnot: /(\{\{desambiguação\}\}|\[\[Categor(?:[ií]a|y):Desambiguaç(ão|ões))/i,
@@ -14158,20 +14158,20 @@ style="text-align:left;"|;"
 		ifhas: /width\s*=/i,
 		sub: [{
 			name: 'width definido em pixels, antes de um style',
-			find: /(\{\|.*?)width\s*=\s*(\d+)(?:px)? (.+?style\s*=\s*["'])/g,
+			find: /(\{\|.*?)width\s*=\s*(\d+)(?:px)? (.*?style\s*=\s*["'])/g,
 			replace: '$1$3width: $2px; '
 		}, {
-			name: 'width definido em outras unidades, antes de um style',
-			find: /(\{\|.*?)width\s*=\s*(\d+(?:%|in|[cme]m|ex|p[tc])) (.+?style\s*=\s*["'])/g,
+			name: 'width definido em certas unidades, antes de um style',
+			find: /(\{\|.*?)width\s*=\s*(\d+(?:%|in|[cme]m|ex|p[tc])) (.*?style\s*=\s*["'])/g,
 			replace: '$1$3width: $2; '
+		}, {
+			name: 'width definido em certas unidades, após um style',
+			find: /(\{\|.*?style\s*=\s*["'])(.*?)\s+width\s*=\s*(\d+(?:%|in|[cme]m|ex|p[tc]))/g,
+			replace: '$1width: $3; $2'
 		}, {
 			name: 'width definido em pixels, após um style',
 			find: /(\{\|.*?style\s*=\s*["'])(.*?)\s+width\s*=\s*(\d+)(?:px)?/g,
 			replace: '$1width: $3px; $2'
-		}, {
-			name: 'width definido em outras unidades, após um style',
-			find: /(\{\|.*?style\s*=\s*["'])(.*?)\s+width\s*=\s*(\d+(?:%|in|[cme]m|ex|p[tc]))/g,
-			replace: '$1width: $3; $2'
 		}]
 	}]
 }] ); // End of APC.addRules()
